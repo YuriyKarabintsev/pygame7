98825b59eb2ebd832e63c48728c37287bc556587
YuriyKarabintsev <urakarabintsev@gmail.com>
2023-02-12 12:41:54 +0700

    Перемещение героя. (задача 1 - файл first_task.py)

--- first_task.py
+++ first_task.py
@@ -3,13 +3,10 @@ import sys
 import pygame
 
 pygame.init()
-#pygame.key.set_repeat(200, 70)
 
 FPS = 50
-WIDTH = 400
-HEIGHT = 300
-
-
+WIDTH = 500
+HEIGHT = 500
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 clock = pygame.time.Clock()
 
@@ -41,10 +38,10 @@ def load_level(filename):
     with open(filename, 'r') as mapFile:
         level_map = [line.strip() for line in mapFile]
 
-    # и подсчитываем максимальную длину    
+    # и подсчитываем максимальную длину
     max_width = max(map(len, level_map))
 
-    # дополняем каждую строку пустыми клетками ('.')    
+    # дополняем каждую строку пустыми клетками ('.')
     return list(map(lambda x: x.ljust(max_width, '.'), level_map))
 
 
@@ -59,7 +56,7 @@ def generate_level(level):
             elif level[y][x] == '@':
                 Tile('empty', x, y)
                 new_player = Player(x, y)
-    # вернем игрока, а также размер поля в клетках            
+    # вернем игрока, а также размер поля в клетках
     return new_player, x, y
 
 
@@ -111,35 +108,6 @@ class Player(pygame.sprite.Sprite):
         self.rect = self.image.get_rect().move(tile_width * pos_x + 15, tile_height * pos_y + 5)
 
 
-class Camera:
-    # зададим начальный сдвиг камеры и размер поля для возможности реализации циклического сдвига
-    def __init__(self, field_size):
-        self.dx = 0
-        self.dy = 0
-        self.field_size = field_size
-
-    # сдвинуть объект obj на смещение камеры
-    def apply(self, obj):
-        obj.rect.x += self.dx
-        # вычислим координату клитки, если она уехала влево за границу экрана
-        if obj.rect.x < -obj.rect.width:
-            obj.rect.x += (self.field_size[0] + 1) * obj.rect.width
-        # вычислим координату клитки, если она уехала вправо за границу экрана            
-        if obj.rect.x >= (self.field_size[0]) * obj.rect.width:
-            obj.rect.x += -obj.rect.width * (1 + self.field_size[0])
-        obj.rect.y += self.dy
-        # вычислим координату клитки, если она уехала вверх за границу экрана
-        if obj.rect.y < -obj.rect.height:
-            obj.rect.y += (self.field_size[1] + 1) * obj.rect.height
-        # вычислим координату клитки, если она уехала вниз за границу экрана
-        if obj.rect.y >= (self.field_size[1]) * obj.rect.height:
-            obj.rect.y += -obj.rect.height * (1 + self.field_size[1])
-
-    # позиционировать камеру на объекте target
-    def update(self, target):
-        self.dx = -(target.rect.x + target.rect.w // 2 - WIDTH // 2)
-        self.dy = -(target.rect.y + target.rect.h // 2 - HEIGHT // 2)
-
 tile_images = {'wall': load_image('box.png'), 'empty': load_image('grass.png')}
 player_image = load_image('mar.png', colorkey=-1)
 
@@ -147,7 +115,6 @@ tile_width = tile_height = 50
 start_screen()
 
 player, level_x, level_y = generate_level(load_level("map.txt"))
-camera = Camera((level_x, level_y))
 STEP = 50
 running = True
 
@@ -157,26 +124,18 @@ while running:
         if event.type == pygame.QUIT:
             running = False
         elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_LEFT:
+            if event.key == pygame.K_LEFT and player.rect.x >= 65:
                 player.rect.x -= STEP
-            if event.key == pygame.K_RIGHT:
+            if event.key == pygame.K_RIGHT and player.rect.x <= 415:
                 player.rect.x += STEP
-            if event.key == pygame.K_UP:
+            if event.key == pygame.K_UP and player.rect.y >= 55:
                 player.rect.y -= STEP
-            if event.key == pygame.K_DOWN:
+            if event.key == pygame.K_DOWN and player.rect.y <= 415:
                 player.rect.y += STEP
-    #
-    camera.update(player)
-
-    for sprite in all_sprites:
-        camera.apply(sprite)
-
     screen.fill(pygame.Color(0, 0, 0))
     tiles_group.draw(screen)
     player_group.draw(screen)
-    #
     pygame.display.flip()
-    #
     clock.tick(FPS)
 
 terminate()
